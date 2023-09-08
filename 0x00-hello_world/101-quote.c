99ac89fbeab7899e5972f0500b293956ad822f74
Amsgold40 <ajibolamoromoke02@gmail.com>
2023-09-07 19:00:12 -0700

    Unix operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,10 @@
  * main - Unix operating system
  * Return: 1 (successful)
  */
+
 int main(void)
 {
 fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, stdout");
+
 return (1);
 }
