3722d825a94b91c7a32fbf3330f99cbebdd70e1f
Amsgold40 <ajibolamoromoke02@gmail.com>
2023-09-18 07:42:16 -0700

    String lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - A function that returns the lenght of a string
+ * @s: string to be counted
+ * Return: lenght of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+		count++;
+	return (count);
+}
